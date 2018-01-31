0b4854d31b77fe33ac4de1e47b89c9f61590b9c0
Håkon Bogen <bogen@beiningbogen.no>
2018-01-31 23:32:42 +0100

    Initial Commit

---
+++ PlaygroundExamples/PlaygroundExamples.h
@@ -0,0 +1,19 @@
+//
+//  PlaygroundExamples.h
+//  PlaygroundExamples
+//
+//  Created by Håkon Bogen on 31/01/2018,5.
+//  Copyright © 2018 Beining & Bogen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlaygroundExamples.
+FOUNDATION_EXPORT double PlaygroundExamplesVersionNumber;
+
+//! Project version string for PlaygroundExamples.
+FOUNDATION_EXPORT const unsigned char PlaygroundExamplesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaygroundExamples/PublicHeader.h>
+
+
