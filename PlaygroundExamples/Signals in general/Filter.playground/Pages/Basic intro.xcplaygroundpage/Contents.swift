ddd14c2a86cdf88428365c29f8625060daac8941
Simen Fonnes <simen@beiningbogen.no>
2018-08-13 14:13:38 +0200

    refac of filter

--- PlaygroundExamples/Signals in general/Filter.playground/Pages/Basic intro.xcplaygroundpage/Contents.swift
+++ PlaygroundExamples/Signals in general/Filter.playground/Pages/Basic intro.xcplaygroundpage/Contents.swift
@@ -3,23 +3,25 @@ import ReactiveSwift
 import ReactiveCocoa
 import Result
 
-// Filter removes entries based on a condition.
-let citySignal = Signal<String, NoError>.pipe()
+//Setup initial values
+let capitalSignal = Signal<String, NoError>.pipe()
+let scandinavianCapitals = ["Oslo", "Copenhagen", "Stockholm", "Helsinki", "Reykjavík", "Tórshavn", "Mariehamn"]
 
-//In this case we remove empty strings.
-let nonEmptyCitySignal = citySignal.output.filter{ text in
-    text != ""  //Only let text that is not empty pass through.
+//Filter removes entries based on a condition.
+//In this case we remove capitals that are not in our Scandinavia array.
+let scandinavicCapitalSignal = capitalSignal.output.filter{ capital in
+    scandinavianCapitals.contains(capital) //Only let scandinavian capitals pass through.
 }
 
-nonEmptyCitySignal.observeValues { (text) in
+//Print the capitals that met the condition defined above.
+scandinavicCapitalSignal.observeValues { (text) in
     print(text)
 }
 
-//These are not printed because they are empty.
-citySignal.input.send(value: "")
-citySignal.input.send(value: "")
+//These are not printed because they are not in the scandinavian array.
+capitalSignal.input.send(value: "Kairo")
+capitalSignal.input.send(value: "Rome")
 
-//These are printed because they are not empty.
-citySignal.input.send(value: "Oslo")
-citySignal.input.send(value: "Kairo")
-citySignal.input.send(value: "Stockholm")
+//These are printed because they are in the scandinavian array.
+capitalSignal.input.send(value: "Oslo")
+capitalSignal.input.send(value: "Stockholm")
