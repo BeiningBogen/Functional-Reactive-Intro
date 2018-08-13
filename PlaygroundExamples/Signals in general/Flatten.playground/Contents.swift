3f9e04ec048cd48bcb7d2e4ce07cd32190fa858e
Simen Fonnes <simen@beiningbogen.no>
2018-08-13 15:22:49 +0200

    added flatten latest

--- PlaygroundExamples/Signals in general/Flatten.playground/Contents.swift
+++ PlaygroundExamples/Signals in general/Flatten.playground/Contents.swift
@@ -11,5 +11,31 @@ import PlaygroundSupport
 
 /*Concat*/
 
+
+
+
 /*Latest*/
+/*
+ Imagine a TV.
+ You get signals from multiple TV channels, but you only want to display the one you are currently watching.
+ */
+
+//These are the multiple channels.
+let channel1Signal = Signal<String, NoError>.pipe()
+let channel2Signal = Signal<String, NoError>.pipe()
+
+//This is the current channel, only displaying the latest signal entered.
+let currentChannelSignal = Signal<Signal<String, NoError>, NoError>.pipe()
+
+//Here we print info displayed from the channels
+currentChannelSignal.output.signal.flatten(.latest).observeValues{value in
+    print(value)
+}
+
+currentChannelSignal.input.send(value: channel1Signal.output.signal)    //You select channel 1
+channel1Signal.input.send(value: "Welcome to channel 1")
+channel1Signal.input.send(value: "Here are some news")
+currentChannelSignal.input.send(value: channel2Signal.output.signal)    //You select channel 2
+channel2Signal.input.send(value: "Welcome to channel 2")
+channel2Signal.input.send(value: "Here is some sport")
 
