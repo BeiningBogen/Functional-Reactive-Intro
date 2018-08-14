b8e72183efb9c035a1ad8a85bc70a8b3c32e6530
Simen Fonnes <simen@beiningbogen.no>
2018-08-14 10:25:06 +0200

    refac scan

--- PlaygroundExamples/Signals in general/Scan.playground/Pages/Basic intro.xcplaygroundpage/Contents.swift
+++ PlaygroundExamples/Signals in general/Scan.playground/Pages/Basic intro.xcplaygroundpage/Contents.swift
@@ -12,22 +12,24 @@ import Result
  */
 
 /*
- Let us say that you receive weather reports from a weather station and you want to combine these values into a csv file.
+ Let us say that you receive an average km per hour from a car every minute.
+ You then want to calculate average between the previous and the new value.
  */
 
-let weatherStationSignal = Signal<String, NoError>.pipe()
+let carSpeedSignal = Signal<Int, NoError>.pipe()
 
-let initialValue = "{location: Oslo, temprature: null}"
+let initialValue = 0
 
-let csvFileSignal = weatherStationSignal.output.scan(initialValue){ combinedValue, newValue in
-    "\(combinedValue),\(newValue)"
+let carAverageSpeedSignal = carSpeedSignal.output.scan(initialValue) { averageSoFar, nextAverage in //We set the initial value to 0 so that the first avg is 0
+    (averageSoFar + nextAverage) / 2    //We calculate the average of the previous average and the new average
 }
 
-csvFileSignal.observeValues{number in
-    print(number)
+carAverageSpeedSignal.observeValues{ averageSpeed in
+    print("\(averageSpeed) km/h")
 }
 
-weatherStationSignal.input.send(value: "{location: Oslo, temprature: 14}")
-weatherStationSignal.input.send(value: "{location: Bergen, temprature: 10}")
-weatherStationSignal.input.send(value: "{location: Trondheim, temprature: 5}")
-weatherStationSignal.input.send(value: "{location: Tønsberg, temprature: 19}")
+carSpeedSignal.input.send(value: 20)    //average of 0 (inital) and 20 is 10
+carSpeedSignal.input.send(value: 30)    //average of 10 and 30 is 20
+carSpeedSignal.input.send(value: 50)
+carSpeedSignal.input.send(value: 40)
+carSpeedSignal.input.send(value: 37)
